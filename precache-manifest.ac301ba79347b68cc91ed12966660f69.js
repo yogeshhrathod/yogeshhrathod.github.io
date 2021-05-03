f269ebe84e7208bb633719b5d77b897b05e627bb
yogeshhrathod <yrathod33@gmail.com>
2021-05-03 21:12:51 +0530

    updated info

---
+++ precache-manifest.ac301ba79347b68cc91ed12966660f69.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c02a954544ee9dc323be38919899146",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "96888fc0d256652fdaa9",
+    "url": "/developerFolio/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "d7358b087961e3bdf724",
+    "url": "/developerFolio/static/css/4.21259c32.chunk.css"
+  },
+  {
+    "revision": "366ee7ccf2f725d62d0f",
+    "url": "/developerFolio/static/css/main.0dd77a08.chunk.css"
+  },
+  {
+    "revision": "2da0e5b09da2e40df6c4",
+    "url": "/developerFolio/static/js/2.4f298c36.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/developerFolio/static/js/2.4f298c36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96888fc0d256652fdaa9",
+    "url": "/developerFolio/static/js/3.01add832.chunk.js"
+  },
+  {
+    "revision": "d7358b087961e3bdf724",
+    "url": "/developerFolio/static/js/4.5c6d3a15.chunk.js"
+  },
+  {
+    "revision": "366ee7ccf2f725d62d0f",
+    "url": "/developerFolio/static/js/main.6f3a3fee.chunk.js"
+  },
+  {
+    "revision": "97cb11f45d703f4c20ab",
+    "url": "/developerFolio/static/js/runtime-main.b0cfc418.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "1089433819161d8e70df907632b36a4a",
+    "url": "/developerFolio/static/media/Pict_logo.10894338.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "058a8f8bc828e94fad0b353e5a262ed8",
+    "url": "/developerFolio/static/media/geospoc.058a8f8b.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/developerFolio/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "d3a26928d532e6ed8d8c872ba6075540",
+    "url": "/developerFolio/static/media/ubisoft.d3a26928.png"
+  }
+]);
